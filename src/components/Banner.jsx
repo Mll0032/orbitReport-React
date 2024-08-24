cd3a61a51b300067d2efd814dfa23553a51724f5
mll0032 <mllawson18@gmail.com>
2024-08-24 17:10:49 -0500

    Updating table to have correct headers and mapping through thr sat data tp grab the correct information to be populated for each row. Import styling.css so the Orbit Report is visually appealing. Added header selector to css to make it look better.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,8 @@
+import './styling.css'
 function Banner() {
     return (
         <div>
-       <header>Orbit Report</header>
+       <header className='flex-container'>Orbit Report</header>
        <p>Click on the buttons to see the satellites in that orbit type</p>
        </div>
     )
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,16 +1,17 @@
+import './styling.css'
 import satData from './satData';
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div className='flex-container'>
-      {displaySats.map((orbitType) => (
-      <button key={orbitType} onclick={() => filterByType(orbitType)}>
-        {orbitType} Orbit
+      {displaySats.map((sat, id) => {
+      return (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
         </button>
-       ))}
-      <button onclick={() => filterByType('All')}>
-        All Orbits
-      </button>
+      );
+    })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
