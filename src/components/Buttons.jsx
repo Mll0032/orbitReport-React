efda6e1f3a31f4ff2d1e8ff4e0dcd35302eaae22
mll0032 <mllawson18@gmail.com>
2024-08-24 15:56:15 -0500

    Update Buttons.jsx so that we populate with buttons for each of the orbits plus an All Orbits button that does not use props and map to create the buttons.

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,16 @@
+import satData from './satData';
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className='flex-container'>
+      {displaySats.map((orbitType) => (
+      <button key={orbitType} onclick={() => filterByType(orbitType)}>
+        {orbitType} Orbit
+        </button>
+       ))}
+      <button onclick={() => filterByType('All')}>
+        All Orbits
+      </button>
     </div>
   );
 };
